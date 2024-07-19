a80a76b43c03731d37d7c714d9a55c8f77c063c8
aefis <alaa-tamri@hotmail.com>
2024-07-19 15:09:30 +0200

    card_6spells

--- card.py
+++ card.py
@@ -1,10 +1,5 @@
 #%%
 
-
-spells = {
-    'attaque_tonnerre' : {'PA' : 1, 'Damage': 3}
-}
-
 #pv : max 100 (spell max : 30)
 #pa : max 10 
 
@@ -15,9 +10,38 @@ class Card:
         self.damage = damage
         self.position = position
 
+#%% spells
 
-# attaque_tonette : Card(2, 3 ,4)
-#%%
+venom = Card(2, 10, [(4, 0), (4, -1), (4, 1), (5, 0), (3, 0), (2, 0), (6, 0)])
 
-venom = Card(2, )
+swords = Card(3, 15, [(2, 0), (2, -1), (2, 1), (3, 0), (3, -1), (3, 1), (4, 0)])
 
+lane_fire = Card(4, 8, [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)])
+
+tsunami = Card(5, 12, [(1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (2, -2), (2, -1), (2, 0), (2, 1), (2, 2)])
+
+sun_burn = Card(4, 10, [(3, 0), (3, -1), (3, 1), (4, 0), (5, 0)])
+
+
+#%% for testing
+
+map = [[' ' for _ in range(10)] for _ in range(5)]
+
+def affiche_map():
+    for i in map:
+        print(i)
+
+def place(pos, spell):
+    for i in spell:
+        try :
+            map[pos[1] + i[1]][pos[0] + i[0]] = 'X'
+        except:
+            pass
+    map[pos[1]][pos[0]] = 'O'
+    affiche_map()
+
+
+
+place((2,3), sun_burn.position)
+
+# %%
