f158955a69b974a1b146a9204097a7e75ddea983
aefis <alaa-tamri@hotmail.com>
2024-07-22 09:37:28 +0200

    card_

--- card.py
+++ card.py
@@ -1,38 +1,54 @@
 #%%
-
+import random
 #pv : max 100 (spell max : 30)
 #pa : max 10 
 
 class Card:
+    list_spells = []
 
-    def __init__(self, pa, damage = 0, position = 0):
+    def __init__(self, name, pa, damage = 0, position = 0):
+        self.name = name
         self.pa = pa
         self.damage = damage
         self.position = position
+        Card.list_spells.append(name)
+
+    @staticmethod
+    def shuffle_3():
+        return random.choices(Card.list_spells, k=3)
+    
+    @staticmethod
+    def shuffle_1():
+        return random.choice(Card.list_spells)
+    
+
+class Attack:
+    def __init__(self, start, duration):
+        self.start = start
+        self.duration = duration
+
 
 #%% spells
 
-venom = Card(2, 10, [(4, 0), (4, -1), (4, 1), (5, 0), (3, 0), (2, 0), (6, 0)])
+venom = Card('venom', 2, 10, [(4, 0), (4, -1), (4, 1), (5, 0), (3, 0), (2, 0), (6, 0)])
 
-swords = Card(3, 15, [(2, 0), (2, -1), (2, 1), (3, 0), (3, -1), (3, 1), (4, 0)])
+swords = Card('swords', 3, 15, [(2, 0), (2, -1), (2, 1), (3, 0), (3, -1), (3, 1), (4, 0)])
 
+fireball = Card('fireball', 5, 10, [(4,0),(4,-1),(4,1),(3,0),(5,0)]) # low impact, Impact --> ball
 
-#%%
-fireball = Card(5, 10, [(4,0),(4,-1),(4,1),(3,0),(5,0)]) # low impact, Impact --> ball
+blizzard = Card('blizzard', 8, 20, [(1,0),(2,0),(3,0),(4,0),(5,0)]) # medium impact, line impact
 
-blizzard = Card(8, 20, [(1,0),(2,0),(3,0),(4,0),(5,0)]) # medium impact, line impact
+tornado = Card('tornado', 10,30,[(5,2),(5,1),(5,3),(4,2),(6,2),(6,3),(6,1),(4,3),(4,1),(3,4),(3,0),(7,4),(7,0)]) # high impact, impact ball, diagonal line
 
-tornado = Card(10,30,[(5,2),(5,1),(5,3),(4,2),(6,2),(6,3),(6,1),(4,3),(4,1),(3,4),(3,0),(7,4),(7,0)]) # high impact, impact ball, diagonal line
+rockfall = Card('rockfall', 9,15,[(2,0),(2,1),(2,2),(2,3),(2,4),(2,-1),(2,-2),(2,-3)])
 
-rockfall = Card(9,15,[(2,0),(2,1),(2,2),(2,3),(2,4),(2,-1),(2,-2),(2,-3)])
+ice_throw = Card('ice_throw', 2,2,[(6,0)])
 
-ice_throw = Card(2,2,[(6,0)])
+lane_fire = Card('lane_fire', 4, 8, [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)])
 
-lane_fire = Card(4, 8, [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)])
+tsunami = Card('tsunami', 5, 12, [(1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (2, -2), (2, -1), (2, 0), (2, 1), (2, 2)])
 
-tsunami = Card(5, 12, [(1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (2, -2), (2, -1), (2, 0), (2, 1), (2, 2)])
-
-sun_burn = Card(4, 10, [(3, 0), (3, -1), (3, 1), (4, 0), (5, 0)])
+sun_burn = Card('sun_burn', 4, 10, [(3, 0), (3, -1), (3, 1), (4, 0), (5, 0)])
 
 
 #%% for testing
@@ -54,6 +70,11 @@ def place(pos, spell):
 
 
 
-place((0,0), ice_throw.position)
+place((0,0), tsunami.position)
 
 # %%
+
+print(Card.shuffle_3())
+
+# %%
+print(Card.list_spells)
